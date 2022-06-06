aa143ab09ddf93a3ff0f4d144807d0df1c2d4968
Studant-Jay <vp.jaeholim@gmail.com>
2022-06-07 08:56:48 +0900

    Add code following by lecture

--- script/clock.js
+++ script/clock.js
@@ -2,5 +2,14 @@
 const clock = document.querySelector("#clock");
 
 
+function getClock(){
+    currentTime = new Date();
+    const hour =String(currentTime.getHours()).padStart(2, "0");
+    const min = String(currentTime.getMinutes()).padStart(2, "0");
+    const sec = String(currentTime.getSeconds()).padStart(2, "0");
 
-clock.innerText("Hi");
+    clock.innerText = `${hour}:${min}:${sec}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
