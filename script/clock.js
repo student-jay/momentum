b3acba7d01cb1af23989511a5ef9a6bb2129caa5
Studant-Jay <vp.jaeholim@gmail.com>
2022-06-07 09:02:30 +0900

    add year information

--- script/clock.js
+++ script/clock.js
@@ -1,14 +1,21 @@
 
-const clock = document.querySelector("#clock");
-
+const clock = document.querySelector("h2#clock");
+const date = document.querySelector("h2#date");
 
 function getClock(){
     currentTime = new Date();
+
+    const year = String(currentTime.getFullYear());
+    const month = String(currentTime.getMonth());
+    const day = String(currentTime.getDay());
     const hour =String(currentTime.getHours()).padStart(2, "0");
     const min = String(currentTime.getMinutes()).padStart(2, "0");
     const sec = String(currentTime.getSeconds()).padStart(2, "0");
 
+    date.innerText = `${day}. ${month}. ${year}`;
     clock.innerText = `${hour}:${min}:${sec}`;
+
+    
 }
 
 getClock();
