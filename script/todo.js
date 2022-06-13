b27936880c8c74939b4fc7067323a4d1963d3002
Studant-Jay <vp.jaeholim@gmail.com>
2022-06-14 07:48:23 +0900

    add function to TodoList : add to storage and load form storage

--- script/todo.js
+++ script/todo.js
@@ -6,19 +6,45 @@ function handleInputForm(event){
     ul = document.querySelector("#todoList")
     
     // Getting text from input tag and assign to li element
-    const task = todoInput.value;
-    li = document.createElement("li");    
-    li.innerText = task;
-    ul.appendChild(li);
-
+    const task = {"task": todoInput.value, "id":Date.now()};
+    taskList.push(task);
+    
+    // Save to localstorage
+    const taskToArrayString = JSON.stringify(taskList);
+    localStorage.setItem("task", taskToArrayString);
+    paintTask(task);
     todoInput.value = "";
+
+    
+}
+
+function paintTask(task){
+    ul = document.querySelector("#todoList")
+    
+    li = document.createElement("li");    
+    li.innerText = task.task;
+    li.id = task.id;
+
+    button = document.createElement("button");
+    button.innerText = "x";
+    li.appendChild(button);
+    console.dir(button);
+    ul.appendChild(li);
 
 }
 
 
-
 const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("input");
+let taskList = [];
 
 todoForm.addEventListener("submit", handleInputForm);
 
+if (localStorage.getItem("task") !== null){
+    const taskListFromStorage = JSON.parse(localStorage.getItem("task"));
+    taskListFromStorage.forEach(element => taskList.push(element));
+    console.log(taskListFromStorage);
+
+    taskListFromStorage.forEach(element => paintTask(element));
+
+}
